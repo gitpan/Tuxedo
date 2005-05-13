4c4714f15e2452d93d19f2d5cfa1440f4ce76fa9
Anthony Fryer <apfryer@hotmail.com>
2005-05-13 05:38:46 +0000

    Import of AFRYER/TUXEDO-2.01 from CPAN.
    gitpan-cpan-distribution: Tuxedo
    gitpan-cpan-version:      2.01
    gitpan-cpan-path:         AFRYER/TUXEDO-2.01.tar.gz
    gitpan-cpan-author:       AFRYER
    gitpan-cpan-maturity:     released

--- TUXEDO.pm
+++ TUXEDO.pm
@@ -267,7 +267,7 @@ require AutoLoader;
     SIGUSR2
     SIGWINCH
 );
-$VERSION = '2.00';
+$VERSION = '2.01';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
